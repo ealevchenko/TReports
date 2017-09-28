c7d70bf58d425909174acb133d105598c55fd45f
ealevchenko <eduard.levchenko@arcelormittal.com>
2017-09-28 12:39:56 +0300

    setup testing

--- EFBF9/Concrete/EFDbContext.cs
+++ EFBF9/Concrete/EFDbContext.cs
@@ -3,8 +3,6 @@ using System.Data.Entity;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EFBF9.Concrete
 {
@@ -20,28 +18,28 @@ namespace EFBF9.Concrete
         //public virtual DbSet<BF9_UnloadMaterial> UnloadMaterial { get; set; }
         //public virtual DbSet<BF9_EnergoSutki> EnergoSutki { get; set; }
 
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            //modelBuilder.Entity<BF9_UnloadBunker>()
-            //    .Property(e => e.Материал)
-            //    .IsFixedLength()
-            //    .IsUnicode(false);
+        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        //{
+        //    //modelBuilder.Entity<BF9_UnloadBunker>()
+        //    //    .Property(e => e.Материал)
+        //    //    .IsFixedLength()
+        //    //    .IsUnicode(false);
 
-            //modelBuilder.Entity<BF9_UnloadBunker>()
-            //    .Property(e => e.Материал_по_факту)
-            //    .IsFixedLength()
-            //    .IsUnicode(false);
+        //    //modelBuilder.Entity<BF9_UnloadBunker>()
+        //    //    .Property(e => e.Материал_по_факту)
+        //    //    .IsFixedLength()
+        //    //    .IsUnicode(false);
 
-            //modelBuilder.Entity<BF9_UnloadMaterial>()
-            //    .Property(e => e.Type)
-            //    .IsFixedLength()
-            //    .IsUnicode(false);
+        //    //modelBuilder.Entity<BF9_UnloadMaterial>()
+        //    //    .Property(e => e.Type)
+        //    //    .IsFixedLength()
+        //    //    .IsUnicode(false);
 
-            //modelBuilder.Entity<BF9_UnloadMaterial>()
-            //    .Property(e => e.Material)
-            //    .IsFixedLength()
-            //    .IsUnicode(false);
-        }
+        //    //modelBuilder.Entity<BF9_UnloadMaterial>()
+        //    //    .Property(e => e.Material)
+        //    //    .IsFixedLength()
+        //    //    .IsUnicode(false);
+        //}
 
     }
 }
--- Testing/Program.cs
+++ Testing/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EFBF9.DataSet;
+using Measurement;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +10,69 @@ namespace Testing
 {
     class Program
     {
+        public class DM_BF9_UnloadBunker
+        {
+            public DateTime datetime { get; set; }
+            public PressureValue davl1 { get; set; }
+        }
+
+        public class BF9_UnloadBunkerDataMeasurement : DataMeasurement<DM_BF9_UnloadBunker>
+        {
+            public BF9_UnloadBunkerDataMeasurement(UnloadBunker obj)
+                : base(obj)
+            {
+
+            }
+
+            public BF9_UnloadBunkerDataMeasurement(List<UnloadBunker> list_obj)
+                : base(list_obj)
+            {
+
+            }
+
+            public override DM_BF9_UnloadBunker Convert(object obj)
+            {
+                return new DM_BF9_UnloadBunker()
+                {
+                    datetime = ((UnloadBunker)obj).Дата_и_время,
+                    davl1 = new PressureValue((double)((UnloadBunker)obj).Точность_рассыпания, "Точность_рассыпания", uPressure.bar, Multiplier.No)
+                };
+            }
+        }
+        
         static void Main(string[] args)
         {
+            //PressureUnit pu = new PressureUnit("Тест параметр давление", uPressure.kgs_m2, Multiplier.No);
+            //FlowUnit fu = new FlowUnit("Тест параметр расход", uFlow.kg_hour, Multiplier.No);
+
+            //List<UnitMeasurement> list = new List<UnitMeasurement>();
+            //list.Add(pu);
+            //list.Add(fu);
+
+            //foreach (UnitMeasurement um in list)
+            //{
+            //    Console.WriteLine("Элемент {0}, класс {1}", um.Description, um.GetType());
+            //}
+
+            //PressureValue pv = new PressureValue(4.5, "Тест параметр давление", uPressure.kgs_m2, Multiplier.No);
+            //FlowValue fv = new FlowValue(1.1, "Тест параметр расход", uFlow.g_hour, Multiplier.No);
+
+            //List<ValueMeasurement> vlist = new List<ValueMeasurement>();
+            //vlist.Add(pv);
+            //vlist.Add(fv);
+
+            //foreach (ValueMeasurement um in vlist)
+            //{
+            //    Console.WriteLine("Элемент {0}, класс {1}, значение {2}, тип {3}", um.Description, um.GetType(), um.Value, um.TypeValue);
+            //}
+
+            EFBF9.Concrete.EFBF9 efdp9 = new EFBF9.Concrete.EFBF9();
+            List<UnloadBunker> list = new List<UnloadBunker>();
+            list = efdp9.GetBF9UnloadBunker(DateTime.Now.AddHours(-5), DateTime.Now);
+            BF9_UnloadBunkerDataMeasurement ubdm = new BF9_UnloadBunkerDataMeasurement(list);
+            BF9_UnloadBunkerDataMeasurement ubdm1 = new BF9_UnloadBunkerDataMeasurement(list[1]);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
