18c2ad6787941765fe9e2711c415cf315c74a93c
ealevchenko <eduard.levchenko@arcelormittal.com>
2017-09-29 14:31:31 +0300

    add App_Localresources

--- TReport/App_LocalResources/Resources.Designer.cs
+++ TReport/App_LocalResources/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TReport.Properties {
+namespace TReport.App_LocalResources {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace TReport.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TReport.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TReport.App_LocalResources.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
