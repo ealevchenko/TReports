c95f14841be7703659c6d1a75e3c48682072b3c0
ealevchenko <eduard.levchenko@arcelormittal.com>
2017-10-20 16:33:17 +0300

    fix bag all object

--- TReport/TREntities/TREnergy.cs
+++ TReport/TREntities/TREnergy.cs
@@ -95,10 +95,11 @@ namespace TReport.TREntities
                {
                    foreach (ItemEnergyFlowDay item in ted.Items.OrderBy(i => i.position))
                    {
-                       foreach (trObj obj in base.trObjs)
+                       trObj obj = base.trObjs.Find(o => o == (trObj)item.trobj);
+                       if (obj != trObj.not)
                        {
-
-                           try {
+                           try
+                           {
                                //Преобразование 
                                DBFlowValue flow = item.flow != null && item.flow.tag > 0 ? (DBFlowValue)list_data_measurement.Find(m => m.id == item.flow.tag).value_measurement : null;
                                item.flow.value = flow != null && item.flow.multiplier != null ? (DBFlowValue)flow.ConvertMultiplier((Multiplier)item.flow.multiplier) : flow;
@@ -126,6 +127,40 @@ namespace TReport.TREntities
                                e.WriteError(String.Format("Ошибка полученния параметра :{0}, объект: {1}", item.name, item.trobj), eventID);
                            }
                        }
+                       else { 
+                           ted.Items.Remove(item); 
+                       }
+                       
+                       //foreach (trObj obj in base.trObjs)
+                       //{
+                       //    try {
+                       //        //Преобразование 
+                       //        DBFlowValue flow = item.flow != null && item.flow.tag > 0 ? (DBFlowValue)list_data_measurement.Find(m => m.id == item.flow.tag).value_measurement : null;
+                       //        item.flow.value = flow != null && item.flow.multiplier != null ? (DBFlowValue)flow.ConvertMultiplier((Multiplier)item.flow.multiplier) : flow;
+
+                       //        DBTempValue avg_temp = item.avg_temp != null && item.avg_temp.tag > 0 ? (DBTempValue)list_data_measurement.Find(m => m.id == item.avg_temp.tag).value_measurement : null;
+                       //        item.avg_temp.value = avg_temp != null && item.avg_temp.multiplier != null ? (DBTempValue)avg_temp.ConvertMultiplier((Multiplier)item.avg_temp.multiplier) : avg_temp;
+
+                       //        DBPressureValue avg_pressure = item.avg_pressure != null && item.avg_pressure.tag > 0 ? (DBPressureValue)list_data_measurement.Find(m => m.id == item.avg_pressure.tag).value_measurement : null;
+                       //        item.avg_pressure.value = avg_pressure != null && item.avg_pressure.multiplier != null ? (DBPressureValue)avg_pressure.ConvertMultiplier((Multiplier)item.avg_pressure.multiplier) : avg_pressure;
+
+                       //        DBPlanimetricValue planimetric = item.planimetric != null && item.planimetric.tag > 0 ? (DBPlanimetricValue)list_data_measurement.Find(m => m.id == item.planimetric.tag).value_measurement : null;
+                       //        item.planimetric.value = planimetric != null && item.planimetric.multiplier != null ? (DBPlanimetricValue)planimetric.ConvertMultiplier((Multiplier)item.planimetric.multiplier) : planimetric;
+
+                       //        DBFlowValue pr_flow = item.pr_flow != null && item.pr_flow.tag > 0 ? (DBFlowValue)list_data_measurement.Find(m => m.id == item.pr_flow.tag).value_measurement : null;
+                       //        item.pr_flow.value = pr_flow != null && item.pr_flow.multiplier != null ? (DBFlowValue)pr_flow.ConvertMultiplier((Multiplier)item.pr_flow.multiplier) : pr_flow;
+
+                       //        DBTimeValue time_norm = item.time_norm != null && item.time_norm.tag > 0 ? (DBTimeValue)list_data_measurement.Find(m => m.id == item.time_norm.tag).value_measurement : null;
+                       //        item.time_norm.value = time_norm != null && item.time_norm.multiplier != null ? (DBTimeValue)time_norm.ConvertMultiplier((Multiplier)item.time_norm.multiplier) : time_norm;
+
+                       //        DBTimeValue time_max = item.time_max != null && item.time_max.tag > 0 ? (DBTimeValue)list_data_measurement.Find(m => m.id == item.time_max.tag).value_measurement : null;
+                       //        item.time_max.value = time_max != null && item.time_max.multiplier != null ? (DBTimeValue)time_max.ConvertMultiplier((Multiplier)item.time_max.multiplier) : time_max;
+                       //    }
+                       //    catch (Exception e)
+                       //    {
+                       //        e.WriteError(String.Format("Ошибка полученния параметра :{0}, объект: {1}", item.name, item.trobj), eventID);
+                       //    }
+                       //}
                    }
                }
            }
